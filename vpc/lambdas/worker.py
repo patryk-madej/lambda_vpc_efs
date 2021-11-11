051d75deafda1d2bf03707c7141c283377a916b2
Patryk Madej <Patryk.Madej@PACONSULTING.COM>
2021-11-11 23:31:21 +0000

    efs in cloudformation

---
+++ vpc/lambdas/worker.py
@@ -0,0 +1,16 @@
+import json
+
+
+def lambda_handler(event, context):
+    print(event)
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
